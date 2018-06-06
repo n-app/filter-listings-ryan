734080df36dc37124430c05edd65d1c217e7af9c
Ryan Tsuzaki <rtsuzaki10@gmail.com>
2018-06-05 22:47:10 -0700

    Added star ratings

--- client/src/components/RoomListEntry.jsx
+++ client/src/components/RoomListEntry.jsx
@@ -1,4 +1,5 @@
 import React from 'React';
+import StarRatings from 'react-star-ratings';
 
 const RoomListEntry = (props) => {
   let slideStyle = {
@@ -11,6 +12,8 @@ const RoomListEntry = (props) => {
       <div className='bedroomNum'>{props.room.numberOfBedrooms} Beds</div>
       <div className='roomName'>{props.room.roomname}</div>
       <div className='price'>${props.room.price} per night</div>
+      <StarRatings rating={props.room.rating} starRatedColor='#008489' numberOfStars={5} starDimension='10px' starSpacing='0px'/>
+      <span className='rating'>{props.room.numberOfReviews}</span>
     </div>
   )
 }
