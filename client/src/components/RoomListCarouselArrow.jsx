f80913bbb427314b24927be69309c61781420fc7
Ryan Tsuzaki <rtsuzaki10@gmail.com>
2018-06-08 12:35:48 -0700

    Fixed main carousel arrows positioning and size

--- client/src/components/RoomListCarouselArrow.jsx
+++ client/src/components/RoomListCarouselArrow.jsx
@@ -1,8 +1,8 @@
 import React from 'React';
 
 const RoomListCarouselArrow = (props) => (
-  <div onClick={props.clickFunction} className={`slide-arrow ${props.direction}`}>
-    <svg> 
+  <div className="inline-block" onClick={props.clickFunction} >
+    <svg className={`slide-arrow ${props.direction}`} viewBox="0 0 18 18" role="img"> 
       {props.arrow}
     </svg>
   </div>
